0287bac82f09574a55b73a5ba7e609d72739e991
mendrit25 <74071551+mendrit25@users.noreply.github.com>
2021-10-26 11:06:33 +0200

    Update

--- utils/Auth.js
+++ utils/Auth.js
@@ -1,5 +1,11 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const psp = require("passport");
+
+const { SECRET } = require("../config");
+const passport = require("../middlewares/passport");
+
 /*
     Register user function (quizer, superadmin, admin)
 */
@@ -45,6 +51,62 @@ const userRegister = async (userDets, role, res) => {
   }
 };
 
+/*
+    Login user function (quizer, superadmin, admin)
+*/
+
+const userLogin = async (userCreds, role, res) => {
+  let { username, password } = userCreds;
+  //First check the username
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(404).json({
+      message: "Username is not found",
+      success: false,
+    });
+  }
+  //Check the role
+  if (user.role !== role) {
+    return res.status(403).json({
+      message: "Please login from the right portal",
+      success: false,
+    });
+  }
+  //If user is valid, check password
+  let isMatch = await bcrypt.compare(password, user.password);
+  if (isMatch) {
+    //Signin the token
+    let token = jwt.sign(
+      {
+        user_id: user._id,
+        role: user.role,
+        username: user.username,
+        email: user.email,
+      },
+      SECRET,
+      { expiresIn: "2 days" }
+    );
+
+    let result = {
+      username: user.username,
+      role: user.role,
+      email: user.email,
+      token: `Bearer ${token}`,
+      expiresIn: 48,
+    };
+    return res.status(200).json({
+      ...result,
+      message: "Yes, you are logged in",
+      success: true,
+    });
+  } else {
+    return res.status(403).json({
+      message: "Incorrect password",
+      success: false,
+    });
+  }
+};
+
 const validateUsername = async (username) => {
   let user = await User.findOne({ username });
   return user ? false : true;
@@ -55,6 +117,38 @@ const validateEmail = async (email) => {
   return user ? false : true;
 };
 
+/*
+  PASSPORT Middleware
+*/
+const userAuth = psp.authenticate("jwt", { session: false });
+
+const serializeUser = (user) => {
+  return {
+    username: user.username,
+    email: user.email,
+    _id: user._id,
+    name: user.name,
+    updatedAt: user.updatedAt,
+    createdAt: user.createdAt,
+  };
+};
+
+/**
+ *  Check role middleware
+ *
+ *
+ */
+
+const checkRole = (roles) => (req, res, next) => {
+  !roles.includes(req.user.role)
+    ? res.status(401).json("Unauthorized")
+    : next();
+};
+
 module.exports = {
+  checkRole,
   userRegister,
+  userLogin,
+  userAuth,
+  serializeUser,
 };
