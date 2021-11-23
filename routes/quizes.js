c07202185ced08d819ab97ae83a1822f34a9919c
Mendrit Arifi <ma47323@ubt-uni.net>
2021-11-23 16:32:11 +0100

    Fixes

--- routes/quizes.js
+++ routes/quizes.js
@@ -75,8 +75,6 @@ router.post(
 );
 
 router.get('/:filename', async (req, res)=>{
-
- 
  gfs.files.findOne({filename:req.params.filename},(err,file)=>{
   const readstream=gfs.createReadStream(file.filename);
   readstream.pipe(res);
