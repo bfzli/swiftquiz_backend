3fbc30652bd817a8446bb377b3c28aea21fd7cb8
Mendrit Arifi <ma47323@ubt-uni.net>
2021-11-24 12:25:52 +0100

    Reading '_id'

--- routes/quizes.js
+++ routes/quizes.js
@@ -28,7 +28,7 @@ router.get(`${prefix}/my-quizzes`, userAuth, async (req, res) => {
 
 router.get(`${prefix}/my-quizzes/:id`, userAuth, async (req, res) => {
   try {
-    const quizById = await Quiz.findById({_id:req.params.id}).populate({
+    const quizById = await Quiz.findById(req.params.id).populate({
       path: "created_by",
       select: "name",
     });
@@ -64,9 +64,9 @@ router.post(`${prefix}/create-quiz`, userAuth, async (req, res) => {
   }
 });
 
-router.delete(`${prefix}/my-quizzes/:_id`, userAuth, async (req, res) => {
+router.delete(`${prefix}/my-quizzes/:id`, userAuth, async (req, res) => {
   try {
-    await Quiz.findByIdAndDelete(req.params._id);
+    await Quiz.findByIdAndDelete(req.params.id);
     return res.status(201).json({
       message: "Quiz deleted successfully !",
       success: true,
