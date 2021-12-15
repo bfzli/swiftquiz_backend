d39dacf2b96cbb5ebc936ecf761c7662481ce015
adnank <adnkas99@gmail.com>
2021-12-15 14:23:18 +0100

    Added Social Authentication Concepts

--- config/index.js
+++ config/index.js
@@ -4,4 +4,12 @@ module.exports = {
   PORT: process.env.APP_PORT,
   SECRET: process.env.APP_SECRET,
   DOMAIN: process.env.APP_DOMAIN,
+  GOOGLE_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  FACEBOOK_ID: process.env.FACEBOOK_CLIENT_ID,
+  FACEBOOK_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
+  LINKEDIN_ID: process.env.LINKEDIN_CLIENT_ID,
+  LINKEDIN_SECRET: process.env.LINKEDIN_CLIENT_SECRET,
+  GITHUB_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_SECRET: process.env.GITHUB_CLIENT_SECRET
 };
--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const mongoose = require("mongoose");
 const { connect } = require("mongoose");
 const { success, error } = require("consola");
 const { DB } = require("./config");
+const cookieSession = require("cookie-session");
+const session = require("express-session");
 
 const PORT = process.env.PORT || 5001;
 
-
-const mongoURI=DB
+const mongoURI = DB;
 
 const conn = mongoose.createConnection(mongoURI);
 
 let gfs;
 conn.once("open", () => {
-  gfs=Grid(conn.db,mongoose.mongo)
-  gfs.collection('uploads')
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
 });
-
 
 //APP INITIALIZING
 const app = exp();
@@ -32,21 +32,32 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/:filename", async (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    const readstream = gfs.createReadStream(file.filename);
+    readstream.pipe(res);
+  });
+});
 
-app.get('/:filename', async (req, res)=>{
- gfs.files.findOne({filename:req.params.filename},(err,file)=>{
- const readstream=gfs.createReadStream(file.filename)
- readstream.pipe(res)
-   })
-})
-
-
+//Session
+app.use(
+  session({
+    secret: "swift",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 //MIDDLEWARE
 app.use(cors());
 app.use(bp.json());
 app.use(passport.initialize());
+app.use(passport.session());
 require("./middlewares/passport")(passport);
+//maxAge = 1day
+// app.use(
+//   cookieSession({ name: "session", keys: "swift", maxAge: 24 * 60 * 60 * 100 })
+// );
 
 // Router Middleware
 app.use("/api/user", require("./routes/users"));
@@ -54,6 +65,7 @@ app.use("/api/user/categories", require("./routes/categories"));
 app.use("/api/user", require("./routes/quizes"));
 app.use("/api/user/pay", require("./routes/payments"));
 app.use("/api/terminal", require("./routes/commands"));
+app.use("/auth", require("./routes/social"));
 
 const startApp = async () => {
   try {
@@ -61,7 +73,7 @@ const startApp = async () => {
     await connect(DB, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      useFindAndModify:false,
+      useFindAndModify: false,
     });
 
     success({
